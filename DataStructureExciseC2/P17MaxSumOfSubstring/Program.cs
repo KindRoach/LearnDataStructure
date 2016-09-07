495e37e2d6da9599264ef7691b44a7ced78c7c69
KindRoach <kindroach@hotmail.com>
2016-09-07 19:29:30 +0800

    Chapter 2

---
+++ DataStructureExciseC2/P17MaxSumOfSubstring/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P17MaxSumOfSubstring
+{
+    class Program
+    {
+        // 找出n个数组成的序列中，和最大的子序列的和（P13）
+        static void Main(string[] args)
+        {
+            int[] numbers = GetNumbers(10000000);
+            DateTime before = new DateTime();
+            DateTime after = new DateTime();
+
+            before = DateTime.Now;
+            Console.WriteLine(GetMaxSum_nlogn(numbers, 0, numbers.Count() - 1));
+            after = DateTime.Now;
+            Console.WriteLine(after - before);
+
+            Console.WriteLine();
+
+            before = DateTime.Now;
+            Console.WriteLine(GetMaxSum_n(numbers));
+            after = DateTime.Now;
+            Console.WriteLine(after - before);
+        }
+
+        static int[] GetNumbers(int size)
+        {
+            int[] numbers = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = random.Next(-1000, 1000);
+            }
+            return numbers;
+        }
+
+        static int GetMaxSum_nlogn(int[] numbers, int left, int right)
+        {
+            if (left == right)
+            {
+                if (numbers[left] > 0)
+                {
+                    return numbers[left];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            int mid = left + (right - left) / 2;
+            int maxSumLeft = GetMaxSum_nlogn(numbers, left, mid);
+            int maxSumRight = GetMaxSum_nlogn(numbers, mid + 1, right);
+
+            int maxBoderSumLeft = 0;
+            int sumLeft = 0;
+            for (int i = mid; i >= left; i--)
+            {
+                sumLeft = sumLeft + numbers[i];
+                maxBoderSumLeft = Math.Max(sumLeft, maxBoderSumLeft);
+            }
+
+            int maxBoderSumRight = 0;
+            int sumRight = 0;
+            for (int i = mid + 1; i <= right; i++)
+            {
+                sumRight = sumRight + numbers[i];
+                maxBoderSumRight = Math.Max(sumRight, maxBoderSumRight);
+            }
+
+            int result = Math.Max(maxSumLeft, maxSumRight);
+            result = Math.Max(result, maxBoderSumLeft + maxBoderSumRight);
+            return result;
+        }
+
+        static int GetMaxSum_n(int[] numbers)
+        {
+            int num = numbers.Count();
+            int thisSum = 0;
+            int maxSum = thisSum;
+            for (int i = 0; i < num; i++)
+            {
+                thisSum = thisSum + numbers[i];
+                if (thisSum > maxSum)
+                {
+                    maxSum = thisSum;
+                }
+                else if (thisSum < 0)
+                {
+                    thisSum = 0;
+                }
+            }
+            return maxSum;
+        }
+    }
+}
---
+++ DataStructureExciseC2/P22BinarySearch/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P22BinarySearch
+{
+    class Program
+    {
+        // 二分查找
+        static void Main(string[] args)
+        {
+            int target = 0;
+            int[] numbers = GetNumbers(100000000, out target);
+            DateTime before = new DateTime();
+            DateTime after = new DateTime();
+
+            before = DateTime.Now;
+            Console.WriteLine(FindIndexOf_n(target, numbers));
+            after = DateTime.Now;
+            Console.WriteLine(after - before);
+
+            Console.WriteLine();
+
+            before = DateTime.Now;
+            Console.WriteLine(FindIndexOf_logn_recursion(target, numbers, 0, numbers.Count() - 1));
+            after = DateTime.Now;
+            Console.WriteLine(after - before);
+
+            Console.WriteLine();
+
+
+            before = DateTime.Now;
+            Console.WriteLine(FindIndexOf_logn(target, numbers));
+            after = DateTime.Now;
+            Console.WriteLine(after - before);
+        }
+
+        static int[] GetNumbers(int size, out int target)
+        {
+            int[] numbers = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = random.Next();
+            }
+            target = random.Next();
+            numbers[random.Next(size)] = target;
+            Array.Sort(numbers);
+            return numbers;
+        }
+
+        static int FindIndexOf_logn(int target, int[] numbers)
+        {
+            int left = 0;
+            int right = numbers.Count() - 1;
+            int mid = 0;
+            while (left <= right)
+            {
+                mid = left + (right - left) / 2;
+                if (target < numbers[mid])
+                {
+                    right = mid;
+                }
+                else if (target == numbers[mid])
+                {
+                    return mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        static int FindIndexOf_n(int target, int[] numbers)
+        {
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                if (target == numbers[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static int FindIndexOf_logn_recursion(int target, int[] numbers, int left, int right)
+        {
+            if (left == right)
+            {
+                if (target == numbers[left])
+                {
+                    return left;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            int mid = left + (right - left) / 2;
+            if (target == numbers[mid])
+            {
+                return mid;
+            }
+            else if (target < numbers[mid])
+            {
+                return FindIndexOf_logn_recursion(target, numbers, left, mid);
+            }
+            else
+            {
+                return FindIndexOf_logn_recursion(target, numbers, mid + 1, right);
+            }
+        }
+    }
+}
