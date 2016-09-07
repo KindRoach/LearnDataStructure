fdedd718115a9b55cdf5141939dfac81016909d5
KindRoach <kindroach@hotmail.com>
2016-09-07 19:26:49 +0800

    Chapters 1

---
+++ DataStructureExciseC1/P9T1.1SelectionProblem/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9T1._1SelectionProblem
+{
+    // 已知n个数，找出第k大（k=n/2）
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = GetNumbers(100000);
+            int[] copyA = (int[])numbers.Clone();
+            int[] copyB = (int[])numbers.Clone();
+            DateTime startTime = DateTime.Now;
+            Console.WriteLine(GetNumerKwithQsort(copyA));
+            DateTime endTime = DateTime.Now;
+            Console.WriteLine(endTime - startTime);
+
+            Console.WriteLine();
+
+            startTime = DateTime.Now;
+            Console.WriteLine(GetNumberKwithHalfSort(copyB));
+            endTime = DateTime.Now;
+            Console.WriteLine(endTime - startTime);
+        }
+
+        private static int[] GetNumbers(int num)
+        {
+            int[] numbers = new int[num];
+            Random random = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                numbers[i] = random.Next();
+            }
+            return numbers;
+        }
+
+        public static int GetNumerKwithQsort(int[] numbers)
+        {
+            int k = numbers.Count() / 2;
+            Array.Sort(numbers);
+            return numbers[k - 1];
+        }
+
+        public static int GetNumberKwithHalfSort(int[] numbers)
+        {
+
+            int k = numbers.Count() / 2;
+            int[] sortedNumber = new int[k];
+            Array.Copy(numbers, 0, sortedNumber, 0, k);
+            Array.Sort(sortedNumber);
+            for (int i = k; i < numbers.Count(); i++)
+            {
+                if (numbers[i] >= sortedNumber[k - 1])
+                {
+                    continue;
+                }
+                for (int j = 0; j < k; j++)
+                {
+                    if (sortedNumber[j] < numbers[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        for (int h = k - 1; h > j; h--)
+                        {
+                            sortedNumber[h] = sortedNumber[h - 1];
+                        }
+                        sortedNumber[j] = numbers[i];
+                        break;
+                    }
+                }
+            }
+            return sortedNumber[k - 1];
+        }
+
+    }
+}
---
+++ DataStructureExciseC1/P9T1.2WordsPuzzle/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9T1._2WordsPuzzle
+{
+    class Program
+    {
+        public static readonly int[] xAdjust = { -1, -1, 0, 1, 1, 1, 0, -1 };
+        public static readonly int[] yAdjust = { 0, -1, -1, -1, 0, 1, 1, 1 };
+
+        static void Main(string[] args)
+        {
+            var chars = new char[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                string input = Console.ReadLine();
+                var inputs = input.Split(' ');
+                for (int j = 0; j < 4; j++)
+                {
+                    chars[i, j] = Convert.ToChar(inputs[j]);
+                }
+            }
+
+            List<string> words = new List<string>();
+            words.Add("this");
+            words.Add("fat");
+            words.Add("two");
+            words.Add("that");
+
+            FindWords(chars, words);
+        }
+
+        public static void FindWords(char[,] chars, List<string> words)
+        {
+            int xLength = chars.GetLength(1);
+            int yLength = chars.GetLength(0);
+            for (int i = 0; i < xLength; i++)
+            {
+                for (int j = 0; j < yLength; j++)
+                {
+                    for (int k = 0; k < 8; k++)
+                    {
+                        StringBuilder word = new StringBuilder(xLength + yLength);
+                        int x = i, y = j;
+                        while (x >= 0 && x < xLength && y >= 0 && y < yLength)
+                        {
+                            word.Append(chars[y, x]);
+                            x = x + xAdjust[k];
+                            y = y + yAdjust[k];
+                        }
+                        foreach (var item in words)
+                        {
+                            if (word.ToString().StartsWith(item))
+                            {
+                                Console.WriteLine(item);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
