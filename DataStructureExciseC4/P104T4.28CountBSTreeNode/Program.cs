1bb4580c5515c2fb7940dfd87317c9fb8b425098
KindRoach <kindroach@hotmail.com>
2016-09-23 21:44:23 +0800

    Chapter4 ++

---
+++ DataStructureExciseC4/P104T4.28CountBSTreeNode/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P104T4._28CountBSTreeNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new BSTree<int>();
+            tree.Insert(3);
+            tree.Insert(7);
+            tree.Insert(4);
+            tree.Insert(9);
+            tree.Insert(1);
+            tree.Insert(12);
+            tree.Insert(-9);
+            Console.WriteLine(CountNode<int>.AllNode(tree.Root));
+            Console.WriteLine(CountNode<int>.AllLeaf(tree.Root));
+            Console.WriteLine(CountNode<int>.AllFullNode(tree.Root));
+        }
+    }
+
+    public static class CountNode<T>
+        where T : IComparable
+    {
+        public static int AllNode(BSTNode<T> root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            return 1 + AllNode(root.LeftChild) + AllNode(root.RightChild);
+        }
+
+        public static int AllLeaf(BSTNode<T> root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            if (root.LeftChild == null && root.RightChild == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return AllLeaf(root.LeftChild) + AllLeaf(root.RightChild);
+            }
+        }
+
+        public static int AllFullNode(BSTNode<T> root)
+        {
+            if (root == null || root.LeftChild == null || root.RightChild == null)
+            {
+                return 0;
+            }
+            return 1 + AllFullNode(root.LeftChild) + AllFullNode(root.RightChild);
+        }
+    }
+
+}
--- DataStructureExciseC4/P80AVLTree/Program.cs
+++ DataStructureExciseC4/P80AVLTree/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace P80AVLTree
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        // 3, 2, 1 ,4 ,5 ,6 ,7 ,16 ,15 ,14 ,13 ,12 ,11, 10, 8, 9
+        private static void Main(string[] args)
         {
+            var A = new int[] { 3, 2, 1, 4, 5, 6, 7, 16, 15, 14, 13, 12, 11, 10, 8, 9 };
+            var tree = new AVLTree<int>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                tree.Insert(A[i]);
+            }
+
+            //tree.Delete(7);
+            //tree.Delete(6);
+
+            tree.Print(AVLTree<int>.PrintType.DLR);
+            tree.Print(AVLTree<int>.PrintType.LDR);
         }
     }
 
@@ -28,10 +37,6 @@ namespace P80AVLTree
             Parent = parent;
             Height = 0;
         }
-
-        public AVLNode()
-        {
-        }
     }
 
     public class AVLTree<T>
@@ -46,6 +51,10 @@ namespace P80AVLTree
         /// <returns></returns>
         public AVLNode<T> Find(T element)
         {
+            if (element == null)
+            {
+                throw new ArgumentNullException();
+            }
             var p = Root;
             while (p != null)
             {
@@ -64,7 +73,6 @@ namespace P80AVLTree
             }
             return null;
         }
-
 
         /// <summary>
         /// null for no element in the tree
@@ -120,10 +128,18 @@ namespace P80AVLTree
             return null;
         }
 
+        /// <summary>
+        /// no change if element already existed
+        /// </summary>
+        /// <param name="element"></param>
         public void Insert(T element)
         {
-            Root = Insert(Root, null, element);
-
+            if (element == null)
+            {
+                throw new ArgumentNullException();
+            }
+            //Root = Insert(Root, null, element);
+            Root = Insert(Root, element);
         }
 
         private short GetHeight(AVLNode<T> node)
@@ -135,6 +151,77 @@ namespace P80AVLTree
             return node.Height;
         }
 
+        private short CalculateHeight(AVLNode<T> node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException("node is null");
+            }
+            return (short)(Math.Max(GetHeight(node.LeftChild), GetHeight(node.RightChild)) + 1);
+        }
+
+        /// <summary>
+        /// no recursion
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        private AVLNode<T> Insert(AVLNode<T> root, T element)
+        {
+            if (root == null)
+            {
+                root = new AVLNode<T>(element, null);
+            }
+            else
+            {
+                var p = root;
+                while (p != null)
+                {
+                    if (element.CompareTo(p.Element) < 0)
+                    {
+                        if (p.LeftChild == null)
+                        {
+                            p.LeftChild = new AVLNode<T>(element, p);
+                        }
+                        p = p.LeftChild;
+                    }
+                    else if (element.CompareTo(p.Element) > 0)
+                    {
+                        if (p.RightChild == null)
+                        {
+                            p.RightChild = new AVLNode<T>(element, p);
+                        }
+                        p = p.RightChild;
+                    }
+                    else
+                    {
+                        return root;
+                    }
+                }
+                while (Math.Abs(GetHeight(p.LeftChild) - GetHeight(p.RightChild)) != 2)
+                {
+                    p = p.Parent;
+                }
+                if (element.CompareTo(p.Element) < 0)
+                {
+                    p = LeftBlance(p);
+                }
+                else
+                {
+                    p = RightBlance(p);
+                }
+            }
+            return root;
+
+        }
+
+        /// <summary>
+        /// recursion version 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="parent"></param>
+        /// <param name="element"></param>
+        /// <returns></returns>
         private AVLNode<T> Insert(AVLNode<T> root, AVLNode<T> parent, T element)
         {
             if (root == null)
@@ -146,57 +233,217 @@ namespace P80AVLTree
                 root.LeftChild = Insert(root.LeftChild, root, element);
                 if (GetHeight(root.LeftChild) - GetHeight(root.RightChild) == 2)
                 {
-                    if (element.CompareTo(root.LeftChild) < 0)
-                    {
-                        root = SingleRotateLeft(root);
-                    }
-                    else
-                    {
-                        root = DoubleRotateLeft(root);
-                    }
+                    root = LeftBlance(root);
                 }
-
             }
             else if (element.CompareTo(root.Element) > 0)
             {
                 root.RightChild = Insert(root.RightChild, root, element);
                 if (GetHeight(root.RightChild) - GetHeight(root.LeftChild) == 2)
                 {
-                    if (element.CompareTo(root.RightChild) < 0)
-                    {
-                        root = SingleRotateRight(root);
-                    }
-                    else
-                    {
-                        root = DoubleRotateRight(root);
-                    }
+                    root = RightBlance(root);
                 }
-
             }
             //else element is already existed in the tree, we do nothing.
-            root.Height = (short)(Math.Max(GetHeight(root.LeftChild), GetHeight(root.RightChild)) + 1);
+            root.Height = CalculateHeight(root);
+            return root;
+        }
+
+        private AVLNode<T> SingleRotateLeft(AVLNode<T> root)
+        {
+            var oldRoot = root;
+            var newRoot = root.LeftChild;
+            newRoot.Parent = oldRoot.Parent;
+            oldRoot.LeftChild = newRoot.RightChild;
+            newRoot.RightChild = oldRoot;
+            oldRoot.Parent = newRoot;
+            oldRoot.Height = CalculateHeight(oldRoot);
+            newRoot.Height = CalculateHeight(newRoot);
+            return newRoot;
+        }
+
+        private AVLNode<T> SingleRotateRight(AVLNode<T> root)
+        {
+            var oldRoot = root;
+            var newRoot = root.RightChild;
+            newRoot.Parent = oldRoot.Parent;
+            oldRoot.RightChild = newRoot.LeftChild;
+            newRoot.LeftChild = oldRoot;
+            oldRoot.Parent = newRoot;
+            oldRoot.Height = CalculateHeight(oldRoot);
+            newRoot.Height = CalculateHeight(newRoot);
+            return newRoot;
+        }
+
+        private AVLNode<T> DoubleRotateLeft(AVLNode<T> root)
+        {
+            root.LeftChild = SingleRotateRight(root.LeftChild);
+            root = SingleRotateLeft(root);
             return root;
         }
 
         private AVLNode<T> DoubleRotateRight(AVLNode<T> root)
         {
-            throw new NotImplementedException();
+            root.RightChild = SingleRotateLeft(root.RightChild);
+            root = SingleRotateRight(root);
+            return root;
         }
 
-        private AVLNode<T> SingleRotateRight(AVLNode<T> root)
+        private AVLNode<T> LeftBlance(AVLNode<T> root)
         {
-            throw new NotImplementedException();
+            var p = root.LeftChild;
+            if (GetHeight(p.LeftChild) > GetHeight(p.RightChild))
+            {
+                root = SingleRotateLeft(root);
+            }
+            else
+            {
+                root = DoubleRotateLeft(root);
+            }
+            return root;
         }
 
-        private AVLNode<T> DoubleRotateLeft(AVLNode<T> root)
+        private AVLNode<T> RightBlance(AVLNode<T> root)
         {
-            throw new NotImplementedException();
+            var p = root.RightChild;
+            if (GetHeight(p.LeftChild) < GetHeight(p.RightChild))
+            {
+                root = SingleRotateRight(root);
+            }
+            else
+            {
+                root = DoubleRotateRight(root);
+            }
+            return root;
         }
 
-        private AVLNode<T> SingleRotateLeft(AVLNode<T> root)
+        /// <summary>
+        /// no change if element not found
+        /// </summary>
+        /// <param name="element"></param>
+        public void Delete(T element)
         {
-            throw new NotImplementedException();
+            if (element == null)
+            {
+                throw new ArgumentNullException();
+            }
+            Root = Delete(Root, element);
         }
 
+        private AVLNode<T> Delete(AVLNode<T> root, T element)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            if (element.CompareTo(root.Element) < 0)
+            {
+                root.LeftChild = Delete(root.LeftChild, element);
+                if (GetHeight(root.RightChild) - GetHeight(root.LeftChild) == 2)
+                {
+                    root = RightBlance(root);
+                }
+            }
+            else if (element.CompareTo(root.Element) > 0)
+            {
+                root.RightChild = Delete(root.RightChild, element);
+                if (GetHeight(root.LeftChild) - GetHeight(root.RightChild) == 2)
+                {
+                    root = LeftBlance(root);
+                }
+            }
+            else
+            {
+                if (root.LeftChild == null)
+                {
+                    return root.RightChild;
+                }
+                else if (root.RightChild == null)
+                {
+                    return root.LeftChild;
+                }
+                else
+                {
+                    var p = FindMax(root.LeftChild);
+                    root.Element = p.Element;
+                    root.LeftChild = Delete(root.LeftChild, p.Element);
+                }
+            }
+            root.Height = CalculateHeight(root);
+            return root;
+        }
+
+        public enum PrintType
+        {
+            /// <summary>
+            /// 前序遍历（Data_Left_Right）
+            /// </summary>
+            DLR,
+
+            /// <summary>
+            /// 中序遍历（Left_Data_Right）
+            /// </summary>
+            LDR,
+
+            /// <summary>
+            /// 后序遍历（Left_Right_Data）
+            /// </summary>
+            LRD
+        }
+
+        public void Print(PrintType type)
+        {
+            switch (type)
+            {
+                case PrintType.DLR:
+                    PrintDLR(Root);
+                    break;
+
+                case PrintType.LDR:
+                    PrintLDR(Root);
+                    break;
+
+                case PrintType.LRD:
+                    PrintLRD(Root);
+                    break;
+
+                default:
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        private void PrintDLR(AVLNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Console.Write($"{node.Element} ");
+            PrintDLR(node.LeftChild);
+            PrintDLR(node.RightChild);
+        }
+
+        private void PrintLDR(AVLNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            PrintLDR(node.LeftChild);
+            Console.Write($"{node.Element} ");
+            PrintLDR(node.RightChild);
+        }
+
+        private void PrintLRD(AVLNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            PrintLRD(node.LeftChild);
+            PrintLRD(node.RightChild);
+            Console.Write($"{node.Element} ");
+        }
     }
 }
