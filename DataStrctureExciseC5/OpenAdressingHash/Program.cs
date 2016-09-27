7ba54aad35536e4383a9e2eda501245e84279da7
KindRoach <kindroach@hotmail.com>
2016-09-27 21:15:59 +0800

    Chapter5

---
+++ DataStrctureExciseC5/OpenAdressingHash/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAdressingHash
+{
+    // 散列（开放地址法）
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 6;
+            var hash = new HashTable<int>(size, x => x % HashTable<int>.NextPrime(size));
+            hash.Insert(3);
+            hash.Insert(3);
+            hash.Insert(10);
+            hash.Insert(17);
+            Console.WriteLine(hash.Find(17).Key);
+            hash.Delete(17);
+            Console.WriteLine(hash.Find(17) == null);
+            return;
+
+
+        }
+    }
+
+    public class HashNode<T>
+    {
+        public enum NodeState
+        {
+            Empty,
+            Used,
+            Deleted
+        }
+        public T Key { get; set; }
+        public NodeState State { get; set; }
+    }
+
+
+    public class HashTable<T>
+    {
+        public int TableSize { get; set; }
+        public int Count { get; set; }
+        public List<HashNode<T>> Nodes { get; set; }
+        public Func<T, int> Hash { get; }
+
+        public static int NextPrime(int x)
+        {
+            while (!IsPrime(x))
+            {
+                x++;
+            }
+            return x;
+        }
+
+        private static bool IsPrime(int x)
+        {
+            if (x <= 0)
+            {
+                throw new ArgumentException("x must > 0");
+            }
+
+            if (x <= 2)
+            {
+                return true;
+            }
+
+            for (int i = 2; i < Math.Sqrt(x) + 1; i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 探测函数
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private static int F(T x, int i)
+        {
+            return i;
+        }
+
+        public HashTable(int tableSize, Func<T, int> hashFunction)
+        {
+            TableSize = NextPrime(tableSize);
+            Nodes = new List<HashNode<T>>(TableSize);
+            for (int i = 0; i < TableSize; i++)
+            {
+                Nodes.Add(new HashNode<T>() { State = HashNode<T>.NodeState.Empty });
+            }
+            Hash = hashFunction;
+            Count = 0;
+        }
+
+        public HashNode<T> Find(T key)
+        {
+            Count++;
+            int hashcode = Hash(key);
+            var p = Nodes[hashcode];
+            int i = 1;
+            while (!p.Key.Equals(key) && p.State != HashNode<T>.NodeState.Empty)
+            {
+                hashcode = (hashcode + F(key, i)) % TableSize;
+                p = Nodes[hashcode];
+                i++;
+            }
+            if (p.State != HashNode<T>.NodeState.Used)
+            {
+                return null;
+            }
+            return p;
+        }
+
+        public void Delete(T key)
+        {
+            var p = Find(key);
+            if (p == null)
+            {
+                return;
+            }
+            p.State = HashNode<T>.NodeState.Deleted;
+        }
+
+        public void Insert(T key)
+        {
+            if (Find(key) != null)
+            {
+                return;
+            }
+            int hashcode = Hash(key);
+            var p = Nodes[hashcode];
+            int i = 1;
+            while (p.State == HashNode<T>.NodeState.Used)
+            {
+                hashcode = (hashcode + F(key, i)) % TableSize;
+                p = Nodes[hashcode];
+                i++;
+            }
+            p.Key = key;
+            p.State = HashNode<T>.NodeState.Used;
+            return;
+        }
+    }
+
+
+}
---
+++ DataStrctureExciseC5/SeparateChainingHash/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateChainingHash
+{
+    // 散列（分离链接法）
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 6;
+            var hash = new HashTable<int>(size, x => x % HashTable<int>.NextPrime(size));
+            hash.Insert(45);
+            hash.Insert(3);
+            hash.Insert(13);
+            hash.Insert(10);
+            hash.Insert(17);
+            hash.Insert(12);
+            hash.Insert(9);
+            Console.WriteLine(hash.Find(3).Key);
+            hash.Delete(3);
+            Console.WriteLine(hash.Find(3) == null);
+            return;
+
+        }
+    }
+
+    public class HashNode<T>
+    {
+        public T Key { get; set; }
+        public HashNode<T> Next { get; set; }
+
+        public HashNode(T key)
+        {
+            Key = key;
+        }
+    }
+
+    public class HashHead<T>
+    {
+        public HashNode<T> Next { get; set; }
+    }
+
+    public class HashTable<T>
+    {
+        public int TableSize { get; set; }
+        public List<HashHead<T>> Heads { get; set; }
+        public Func<T, int> Hash { get; }
+
+        public static int NextPrime(int x)
+        {
+            while (!IsPrime(x))
+            {
+                x++;
+            }
+            return x;
+        }
+
+        private static bool IsPrime(int x)
+        {
+            if (x <= 0)
+            {
+                throw new ArgumentException("x must > 0");
+            }
+
+            if (x <= 2)
+            {
+                return true;
+            }
+
+            for (int i = 2; i < Math.Sqrt(x) + 1; i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public HashTable(int tableSize, Func<T, int> hashFunction)
+        {
+            TableSize = NextPrime(tableSize);
+            Heads = new List<SeparateChainingHash.HashHead<T>>(TableSize);
+            for (int i = 0; i < TableSize; i++)
+            {
+                Heads.Add(new HashHead<T>());
+            }
+            Hash = hashFunction;
+        }
+
+        /// <summary>
+        /// null for key not found
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public HashNode<T> Find(T key)
+        {
+            int hashcode = Hash(key);
+            var p = Heads[hashcode].Next;
+            while (p != null && !p.Key.Equals(key))
+            {
+                p = p.Next;
+            }
+            return p;
+        }
+
+        /// <summary>
+        /// return null for key not found, pre is null for the first node
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="pre"></param>
+        /// <returns></returns>
+        public HashNode<T> FindWithPre(T key, out HashNode<T> pre)
+        {
+            int hashcode = Hash(key);
+            var p = Heads[hashcode].Next;
+            pre = null;
+            while (p != null && !p.Key.Equals(key))
+            {
+                pre = p;
+                p = p.Next;
+            }
+            return p;
+        }
+
+        /// <summary>
+        /// no change if key not found
+        /// </summary>
+        /// <param name="key"></param>
+        public void Delete(T key)
+        {
+            HashNode<T> pre;
+            var p = FindWithPre(key, out pre);
+            if (p == null)
+            {
+                return;
+            }
+
+            // p is not the first node
+            if (pre != null)
+            {
+                pre.Next = p.Next;
+                return;
+            }
+
+            // p is the first node
+            int hashcode = Hash(p.Key);
+            Heads[hashcode].Next = p.Next;
+            return;
+        }
+
+        /// <summary>
+        /// return true for success , return false if key already existed and this method make no change
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool Insert(T key)
+        {
+            if (Find(key) != null)
+            {
+                return false;
+            }
+
+            int hashcode = Hash(key);
+            var node = new HashNode<T>(key);
+            var head = Heads[hashcode];
+            node.Next = head.Next;
+            head.Next = node;
+            return true;
+        }
+
+    }
+}
